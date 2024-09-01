faacfe63e277706cfaf94898fcc4aa9d51aa44ca
gonpsk <160533788+gonpsk@users.noreply.github.com>
2024-09-01 13:52:09 +0700

    Update page.jsx

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 
 import { useUserAuth } from "../context/UserAuthContext";
 
-function Page() {
+function page() {
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -116,4 +116,4 @@ function Page() {
   );
 }
 
-export default Page;
+export default page;
